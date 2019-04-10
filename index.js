e07ab8bf10759ccbf207eff2053aed933081bcd8
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-04-10 15:34:21 -0400

    - updated node version and npm outdated

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const httpServer = require('./lib/http-server');
 const httpsServer = require('./lib/https-server');
 
-module.exports = (httpsPort, behindProxy, httpsOptions, httpsApp) => {
-    const returnValue = {
-        httpServer: httpServer(httpsPort, behindProxy),
-        httpsServer: httpsServer(httpsOptions, httpsApp)
-    };
-
-    Object.freeze(returnValue);
-
-    return returnValue;
-};
+module.exports = (httpsPort, behindProxy, httpsOptions, httpsApp) => Object.freeze({
+    httpServer: httpServer(httpsPort, behindProxy),
+    httpsServer: httpsServer(httpsOptions, httpsApp)
+});
